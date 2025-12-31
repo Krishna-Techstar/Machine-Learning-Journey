e2e6506eaf1a62cbfac1b24cf2eb991fea58282c
Krishnakant Sharma <139650964+Krishna-Techstar@users.noreply.github.com>
2025-12-31 23:41:02 +0530

    last Push of code of 2025

---
+++ Python-Basics/Fundamentals_2/01-if.py
@@ -0,0 +1,10 @@
+# voter id card 
+
+print("Enter Your Age :")
+age =int( input("age"))
+
+if age >= 18:
+    print("You can Vote")
+    print("You can Drive")
+else:
+    print("You can't Vote")
---
+++ Python-Basics/Fundamentals_2/02-elif.py
@@ -0,0 +1,10 @@
+color = input("Enter Color ")
+
+if color == "red":
+    print("Stop")
+elif color == "green":
+    print("Go")
+elif color == "yellow":
+    print("Wait")
+else:
+    print("You have Entry Wrong Color")

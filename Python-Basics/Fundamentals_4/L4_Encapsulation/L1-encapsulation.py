6f5f0cde6f6e298c2973f8dcb2b792c18c796368
Krishnakant Sharma <139650964+Krishna-Techstar@users.noreply.github.com>
2026-04-29 23:35:06 +0530

    Inheritance

--- Python-Basics/Fundamentals_4/L4_Encapsulation/L1-encapsulation.py
+++ Python-Basics/Fundamentals_4/L4_Encapsulation/L1-encapsulation.py
@@ -3,7 +3,15 @@ class BankAccount:
         self.name = name #public
         self._balance = balance #private - data mangaing
 
+    def get_balance(self):
+        return self.__balance 
+    
+    def set_balance(self, newBalance):
+        self.__balance = newBalance
+
 acc1 = BankAccount("Krishna", 10_00000)
 
-print(acc1.name, acc1._balance)
+
+acc1.set_balance(80000)
+print(acc1.name, acc1.get_balance())
 
